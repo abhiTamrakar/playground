53d9c73037b8e8acd4de6bad8741790373da708b
Abhishek Tamrakar <abhishek.tamrakar08@gmail.com>
2018-09-27 19:02:36 +0530

    Update tic-tac-toe.sh

--- bash_games/tic-tac-toe.sh
+++ bash_games/tic-tac-toe.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
-
+# AUTHOR: Abhishek Tamrakar
+# EMAIL: abhishek.tamrakar08@gmail.com
+#	LICENSE: Copyright (C) 2018 Abhishek Tamrakar
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+##
 # tic-tac-toe game for shell users
 
 # board design
